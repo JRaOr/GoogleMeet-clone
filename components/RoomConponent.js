6ae977eecf157e0ee44a1afaea6899d7a60d3eee
Gerardo Ramirez <gerardo.ramirez@viaro.net>
2022-05-13 09:53:00 -0500

    Other responsive things

--- components/RoomConponent.js
+++ components/RoomConponent.js
@@ -35,7 +35,7 @@ export default function RoomComponent( { room, participants, track, user, showCh
         )
     }
     return(
-        <div className={`participants-grid gap-2 mx-auto ${people < 7 ? 'max-w-[1050px]':'max-w-[1545px]'} relative ${people < 3 ? `participants-${people}`: people < 7 ? 'participants-3' : 'participants-4'}`}>
+        <div className={`participants-grid flex flex-col md:grid overflow-scroll py-10 md:py-0 hide-scroll gap-2 mx-auto ${people < 7 ? 'max-w-[1050px]':'max-w-[1545px]'} relative ${people < 3 ? `participants-${people}`: people < 7 ? 'participants-3' : 'participants-4'}`}>
             {participants.map((participant, index)=> {
                 if( people > 9 && index > 6 ) return null
                 return(
@@ -43,7 +43,7 @@ export default function RoomComponent( { room, participants, track, user, showCh
                 )
             })}
             {people > 9 && <MultipleParticipants participants={participants.slice(7, participants.length)}/>}
-            {participants.length > 0 ? <div className='rounded-md overflow-hidden bg-[#36373a] flex items-center justify-center h-full w-full relative max-w-[504px] max-h-[283px] my-auto mx-auto'>
+            {participants.length > 0 ? <div className='rounded-md overflow-hidden bg-[#36373a] flex items-center justify-center w-full relative max-w-[504px] h-[283px] my-auto mx-auto'>
                 {_videoTrack()}
                 <p className="z-[3] absolute text-white text-xl left-2 bottom-2">Tú</p>
             </div> : null}
